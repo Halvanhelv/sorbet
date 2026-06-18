d2ae2a7b16446bdecccea9b2c83d1d652148b6dc
Trevor Elliott <awesomelyawesome@gmail.com>
2026-06-18 14:54:57 -0700

    Support both classic and test-packages

--- packager/GenPackages.cc
+++ packager/GenPackages.cc
@@ -50,7 +50,7 @@ void exportClassOrModule(const core::GlobalState &gs,
             auto &referencingPkgInfo = gs.packageDB().getPackageInfo(packageForF);
             auto *imp = referencingPkgInfo.importsPackage(owningPackage);
             if (imp != nullptr && imp->usesInternals) {
-                ENFORCE(gs.packageDB().testPackages());
+                ENFORCE(referencingPkgInfo.usesTestPackages);
                 continue;
             }
         }
@@ -84,7 +84,7 @@ void exportField(const core::GlobalState &gs,
             auto &referencingPkgInfo = gs.packageDB().getPackageInfo(packageForF);
             auto *imp = referencingPkgInfo.importsPackage(owningPackage);
             if (imp != nullptr && imp->usesInternals) {
-                ENFORCE(gs.packageDB().testPackages());
+                ENFORCE(referencingPkgInfo.usesTestPackages);
                 continue;
             }
         }
@@ -123,7 +123,7 @@ vector<core::packages::Import> computeNewImports(const core::GlobalState &gs,
     // !packageReferenceInfo.importNeeded or packageReferenceInfo.causesModularityError, as well if the import would
     // cause a visibility error. Maybe the common helper could take a function that filters?
     for (auto &[file, referencedPackages] : pkgInfo.packagesReferencedByFile) {
-        auto importType = core::packages::PackageInfo::fileToImportType(gs, file);
+        auto importType = pkgInfo.fileToImportType(gs, file);
         for (auto &[packageName, packageReferenceInfo] : referencedPackages) {
             auto &pkgInfo = gs.packageDB().getPackageInfo(packageName);
             if (!pkgInfo.exists()) {
@@ -309,8 +309,7 @@ void GenPackages::run(core::GlobalState &gs) {
                     auto &referencedPackageInfo = gs.packageDB().getPackageInfo(referencedPackageName);
                     ENFORCE(referencedPackageInfo.exists());
                     if (gs.packageDB().updateVisibilityFor(referencedPackageName)) {
-                        if (!referencedPackageInfo.isVisibleTo(
-                                gs, pkgInfo, core::packages::PackageInfo::fileToImportType(gs, file)) ||
+                        if (!referencedPackageInfo.isVisibleTo(gs, pkgInfo, pkgInfo.fileToImportType(gs, file)) ||
                             referencedPackageInfo.visibleToEverything()) {
                             // either:
                             // - it is a visibility error for pkgName to reference referencedPackageName, and we want to
